bab94325e87e089ac769602106ecc4456e528fa3
Gorish Gupta <gorishgupta50@gmail.com>
2025-07-15 23:42:50 +0530

    Update DS3136.java

--- DS3136.java
+++ DS3136.java
@@ -1,26 +1,28 @@
 class DS3136 {
     public boolean isValid(String word) {
-        if(word.length() < 3){
+        if (word.length() < 3) {
             return false;
-        }        
+        }
         boolean hasVowel = false;
         boolean hasConsonant = false;
-        for(char c : word.toCharArray()){
-            if(Character.isLetter(c)){
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
                 char ch = Character.toLowerCase(c);
-                if(ch =='a' || ch =='o' ||ch =='i' ||ch =='e' ||ch =='u'){
+                if (ch == 'a' || ch == 'o' || ch == 'i' || ch == 'e' || ch == 'u') {
                     hasVowel = true;
-                }
-                else{
+                } else {
                     hasConsonant = true;
                 }
+            } else if (!Character.isDigit(c)) {
+                return false;
             }
-                else if(!Character.isDigit(c)){
-                    return false;
-                }
-            }
-        
-        return hasVowel && hasConsonant;    
+        }
+
+        return hasVowel && hasConsonant;
     }
 
+    public static void main(String[] args) {
+        DS3136 ds3136 = new DS3136();
+        System.out.println(ds3136.isValid("hello123")); // Output: true
+    }
 }
