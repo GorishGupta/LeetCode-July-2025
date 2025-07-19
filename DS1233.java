3f7291dc438461a7c0094c4f32e5fa69120ca57d
Gorish Gupta <gorishgupta50@gmail.com>
2025-07-19 22:40:17 +0530

    Create DS1233.java

---
+++ DS1233.java
@@ -0,0 +1,20 @@
+class DS1233 {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> ans = new ArrayList<>();
+        ans.add(folder[0]);
+        for (int i = 1; i < folder.length; i++) {
+            String lastFolder = ans.get(ans.size() - 1) + "/";
+            if (!folder[i].startsWith(lastFolder)) {
+                ans.add(folder[i]);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        DS1233 ds1233 = new DS1233();
+        int[] nums = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(ds1233.removeSubfolders(nums));
+    }
+}
