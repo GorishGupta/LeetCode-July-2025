c1acbba6aeaa2f48711dfdf743d55a6b630590f6
Gorish Gupta <gorishgupta50@gmail.com>
2025-07-30 22:55:35 +0530

    Update DS2419.java

--- DS2419.java
+++ DS2419.java
@@ -1,10 +1,9 @@
-class DS2419 {
+public class DS2419 {
     public int longestSubarray(int[] nums) {
         int max = 0;
         for (int i = 0; i < nums.length; i++) {
             max = Math.max(max, nums[i]);
         }
-
         int i = 0;
         int j = 0;
         int ans = 0;
@@ -25,6 +24,8 @@ class DS2419 {
     public static void main(String[] args) {
         DS2419 ds = new DS2419();
         int[] nums = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9 };
-        System.out.println(ds.longestSubarray(nums)); // Output: 10
-    }
+        System.out.println(ds.longestSubarray(nums)); 
+        // Example output: 10
+        int[] nums2 = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10 };
+        System.out.println(ds.longestSubarray(nums2));
 }
